cf3334f39c2052f0012f77142d4127f947627d6b
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2024-01-06 19:26:52 -0500

    Update date.js

--- services/date.js
+++ services/date.js
@@ -13,7 +13,7 @@ module.exports.addDateOfBirthAndZodiac = (id, dob) => {
       .exec()
       .then((profile) => {
         console.log("here in zodiac")
-        let date = parseInt(dob.slice(8));
+        let date = parseInt(dob.slice(9, 11));
         console.log("date ", date)
         let month = parseInt(dob.slice(6, 8));
          console.log("month ", month)
